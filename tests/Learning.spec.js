4f8ed7d950130aee5656d157713bc7a419f94ec2
Murugan Kanakkanadar <murugan.kanakkanadar@expleogroup.com>
2025-04-09 18:19:55 +0530

    windows handling ends with error

--- tests/Learning.spec.js
+++ tests/Learning.spec.js
@@ -1,4 +1,5 @@
-const {test, expect} = require('@playwright/test');
+const {test, expect, chromium} = require('@playwright/test');
+const exp = require('constants');
 test("Handling dynamic drop down",async({page})=>
 {
     const departSearch = page.locator("#input-with-icon-adornment");
@@ -7,6 +8,26 @@ test("Handling dynamic drop down",async({page})=>
     await departSearch.clear();
     await departSearch.pressSequentially("New", {delay:1000});
     await page.locator("div[class='MuiBox-root css-134xwrj'] >> text=New Haven").nth(0).click();
+    await page.pause();
+});
 
-    //new branch
+test.only("windows handling", async()=>
+{
+    // creating multiple pages
+    const browser = await chromium.launch();
+    const context = await browser.newContext();
+    
+    const page1 = await context.newPage();
+   // const page2 = await context.newPage();
+
+    await page1.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    await expect(page1).toHaveTitle("OrangeHRM");
+
+    // await page2.goto("https://demo.cyclos.org/ui/home");
+    // await expect(page2).toHaveTitle("Home - Cyclos");
+
+    const newPage = context.waitForEvent('page');
+    await page1.locator("a[href='http://www.orangehrm.com']").click();
+    await expect(newPage).toHaveTitle("Human Resources Management Software | OrangeHRM HR Software ");
+
 });
